5e139bea996bbce3416fa7499bf7c21fed9555ef
Ismail Ajiev <iadjiev@gmail.com>
2020-10-14 13:52:42 +0300

    Комментарии от ревьюера

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+// Можно path.join(__dirname, 'static');
+// Плюс такого подхода в том, что join сам поставит нужные разделители между дирректориями в зависимости от ОС
 app.use(express.static(__dirname + '/static'));
 
 app.listen(PORT, function () {
-	console.log(`App listening on port ${PORT}!`);
+  console.log(`App listening on port ${PORT}!`);
 });
--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,39 @@
-let button = document.getElementById('submit-button');
-button.onclick = function () {
-	let formName = this.dataset.formName,
-		form = document.forms[formName],
-		formData = new FormData(form),
-		data = {};
-	for (let key of formData.keys()) {
-		data[key] = formData.get(key);
+// Советую по умолчанию использовать const, а let только если очень надо
+/*
+	Например в такой ситуации
+
+	let answer = null;
+
+	if (isValid()) {
+		answer = 42;
+		someLogic();
+	} else {
+		answer = 24;
+		someAnotherLogic();
 	}
-	console.log(data)
+
+	...
+*/
+let button = document.getElementById('submit-button');
+
+// Для навешивания обработчиков событий лучше всего использовать addEventListener
+button.onclick = function () {
+  // Лучше не пользоваться таким синтаксисом, а объявлять переменные по одной.
+  // Во-первых это плохо читается
+  // Во-вторых, в некоторых ситуациях, браузерные движки могут неверно распарсить такое выражение
+  let formName = this.dataset.formName,
+    form = document.forms[formName],
+    formData = new FormData(form),
+    data = {};
+  for (let key of formData.keys()) {
+    data[key] = formData.get(key);
+  }
+  console.log(data);
 };
 
 let avatar = document.getElementById('image-input'),
-	imageInput = document.getElementById('avatar');
+  imageInput = document.getElementById('avatar');
 
 avatar.onclick = function () {
-	imageInput.click();
+  imageInput.click();
 };
